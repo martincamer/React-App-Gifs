c4130be213e81e71a690a8aeaac7ca5fd1a66dab
martincamer011 <martincamer011@gmail.com>
2022-03-30 19:20:45 -0300

    new-commit

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { useFetchGifs } from "../hooks/useFetchGifs";
 import GifGridItem from "../components/GifGridItem";
+
 export const GifGrid = ({ category }) => {
   const { data, loading } = useFetchGifs(category);
 
   return (
     <>
-      <h3 className="category-h1">{category}</h3>
+      <h3 className="category-h1 animate__animated animate__flipInY ">
+        {category}
+      </h3>
 
       {loading && <p className="center-loading">Loading ......</p>}
 
-      <div className="card-grid">
+      <div className="card-grid ">
         {data.map((img) => (
           <GifGridItem key={img.id} {...img}></GifGridItem>
         ))}
